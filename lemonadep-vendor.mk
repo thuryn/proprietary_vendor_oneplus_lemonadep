00bacf26bc89c28244fe0c04393cc938233f4bcc
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 05:01:14 +0200

    lemonadep: Remove prebuilt audio HAL
    Change-Id: Idb587844cbfe5a1eb01d0756b05f159f5fa58eb1

--- lemonadep-vendor.mk
+++ lemonadep-vendor.mk
@@ -704,9 +704,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/lemonadep/proprietary/vendor/firmware/vpu20_4v_unsigned.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/vpu20_4v_unsigned.mbn
 
 PRODUCT_PACKAGES += \
-    audio.primary.lahaina \
-    libextcamera_client \
-    vendor.oplus.hardware.extcamera@1.0 \
     com.qti.camx.chiiqutils \
     com.qti.eisv2 \
     com.qti.eisv3 \
@@ -842,12 +839,9 @@ PRODUCT_PACKAGES += \
     libDeVIS \
     libdehaze \
     libfastov_dsp \
-    liba2dpoffload_odm \
-    libaudioEngineerTest \
     liblvimfs \
     liblvimfs_wrapper \
     libnightvision \
-    libssrec_odm \
     anc.hal \
     android.hardware.secure_element@1.0-impl \
     com.qti.sensor.gc02m1b.lemonade \
